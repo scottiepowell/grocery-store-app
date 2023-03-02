9e148499d6a54b389632a8a5dd07186aa8187818
scottiepowell <scottiepowell@gmail.com>
2023-03-02 16:24:29 -0700

    Delete app/tests directory

--- app/tests/test_app.py
+++
@@ -1,6 +0,0 @@
-from .. import app
-
-def test_valid():
-    assert app.valid('example@example.com', 'password') == True
-    assert app.valid('notanemail', 'password') == False
-    assert app.valid('example@example.com', 'wrongpassword') == False
