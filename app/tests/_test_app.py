476c64348cbf457c58d29c26397c4f5f244f133a
scottiepowell <scottiepowell@gmail.com>
2023-03-02 16:23:00 -0700

    Create _test_app.py

---
+++ app/tests/_test_app.py
@@ -0,0 +1,6 @@
+from .. import app
+
+def test_valid():
+    assert app.valid('example@example.com', 'password') == True
+    assert app.valid('notanemail', 'password') == False
+    assert app.valid('example@example.com', 'wrongpassword') == False
